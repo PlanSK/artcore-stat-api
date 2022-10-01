c54e3beecae7604779df2550bbfe63a31b829e93
PlanSK <skapkanshchikov@gmail.com>
2022-10-02 00:05:59 +0300

    Add pydantic, mypy. Using pydantic.BaseModel.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import json
+from typing import Union
 
 import requests
 from bs4 import BeautifulSoup
+from pydantic import BaseModel
 
 from settings import PC, CONSOLE, URL, Zone
+
+
+class GameZone(BaseModel):
+    id: int
+    name: str
+    all_items: int
+    busy_items: int
+    free_items: int
+
+class ZonesLoadData(BaseModel):
+    pc_zone: list[GameZone]
+    console_zone: list[GameZone]
 
 
 def get_stat_page(url: str) -> str:
@@ -12,19 +25,19 @@ def get_stat_page(url: str) -> str:
     """
     request = requests.get(url)
     request.encoding = 'utf-8'
-    if request.status_code == 200:
-        return request.text
-    raise ValueError('Status code is not 200')
+    if request.status_code != 200:
+        raise ValueError('Status code is not 200')
+    return request.text
 
 
-def get_current_loads(params: Zone, bs_object: BeautifulSoup) -> dict:
+def get_current_loads(zone: Zone, bs_object: BeautifulSoup) -> GameZone:
     """
     Return dict with load of zone
     """
-    limit = bs_object.find_all('p', params.tag)
+    limit = bs_object.find_all('p', zone.tag)
     tags_set = set(
         tag for tag in limit
-        if int(tag.get_text(strip=True)) in params.items_range
+        if int(tag.get_text(strip=True)) in zone.items_range
     )
     all_count = len(tags_set)
     busy_items = [
@@ -33,14 +46,16 @@ def get_current_loads(params: Zone, bs_object: BeautifulSoup) -> dict:
                 tag.img.attrs.get('src').find('red') >= 0)
     ]
     busy_count = len(busy_items)
-    return {
-        'all': all_count,
-        'busy': busy_count,
-        'free': all_count - busy_count,
-    }
+    return GameZone(
+        id=zone.id,
+        name=zone.name,
+        all_items=all_count,
+        busy_items=busy_count,
+        free_items=all_count - busy_count,
+    )
 
 
-def get_zone_data(url: str) -> str:
+def get_zone_data(url: str) -> Union[ZonesLoadData, None]:
     """
     Return JSON with loads data PC and CONSOLE zones
     or None if url address is not available.
@@ -50,22 +65,14 @@ def get_zone_data(url: str) -> str:
     except:
         return None
     bs_object = BeautifulSoup(html, "lxml")
-    pc_dict = {}
-    console_dict = {}
-    for title, params in PC.items():
-        pc_dict.update({
-            title: get_current_loads(params=params, bs_object=bs_object)
-        })
-    for title, params in CONSOLE.items():
-        console_dict.update({
-            title: get_current_loads(params=params, bs_object=bs_object)
-        })
-    return json.dumps(
-        {
-            'PC': pc_dict,
-            'CONSOLE': console_dict,
-        }
-    )
+    pc_zone = [
+        get_current_loads(zone=zone, bs_object=bs_object) for zone in PC
+    ]
+    console_zone = [
+        get_current_loads(zone=zone, bs_object=bs_object) for zone in CONSOLE
+    ]
+
+    return ZonesLoadData(pc_zone=pc_zone, console_zone=console_zone)
 
 
 if __name__ == "__main__":
